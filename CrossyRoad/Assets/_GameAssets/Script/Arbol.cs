8c3a023b99688fbd3786b83296a4ff1b7f161b78
Jepe <unityjepe@gmail.com>
2018-10-01 13:44:40 +0200

    Initial commit

---
+++ CrossyRoad/Assets/_GameAssets/Script/Arbol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arbol : MonoBehaviour {
+    AudioSource audioSource;
+
+    
+	// Use this for initialization
+	void Start () {
+
+        audioSource= GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play();
+        Destroy(other.gameObject);
+    }
+}
---
+++ CrossyRoad/Assets/_GameAssets/Script/Pollo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pollo : MonoBehaviour {
+    private Rigidbody rb;
+    [SerializeField] int fuerza = 400;
+    [SerializeField] Transform pospies;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	private void Awake()
+    {
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Collider[] cols = Physics.OverlapSphere(pospies.position, 0.1f, LayerMask.NameToLayer("Terrain"));
+
+            for(int i=0; i< cols.Length; i++)
+            {
+                Debug.Log("Estoy tocando con los pies en algo:" + i);
+            }
+            rb.AddForce(new Vector3 (1, 1, 0) * fuerza);//se mueve delante
+            
+        }
+        else if (Input.GetKeyDown("l"))
+        {
+
+            
+            rb.AddForce(new Vector3(0, 1, 1) * fuerza);//se mueve lateralIzq
+        }
+        else if (Input.GetKeyDown("i"))
+        {
+
+
+            rb.AddForce(new Vector3(-1, 1, 0) * fuerza);//se mueve atras
+        }
+        else if (Input.GetKeyDown("j"))
+        {
+
+
+            rb.AddForce(new Vector3(0, 1, -1) * fuerza);//se mueve lateralDer
+        }
+    }
+}
